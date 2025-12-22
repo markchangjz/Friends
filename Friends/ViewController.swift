50a5f076ac3e67c183ab5264ff0957da71d3e9dd
Mark Chang <shea4a71@gmail.com>
2025-12-22 12:44:12 +0800

    Initial Commit

---
+++ Friends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Friends
+//
+//  Created by Mark Chang on 2025/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
